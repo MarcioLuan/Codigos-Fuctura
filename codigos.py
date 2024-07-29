7eec1d83f8a2e23395c829bd4af0854b4fdb2f3c
Márcio Luan <marcioluan18@hotmail.com>
2024-07-28 22:38:05 -0300

    Create codigos.py

---
+++ codigos.py
@@ -0,0 +1,19 @@
+# Para comentar uma linha você deve usar o jogo fa velha no inicio da linha
+
+"""
+    Para comentar mais de uma linha:
+    Usa 3 aspas simples ou duplas para abrir
+    e fecha com 3 aspas (da mesma forma que abriu)
+"""
+
+#Cuidado, o programa não entende a linguagem puramente humana
+
+se voce digitar e não simbolizar que é um comentário o programa não vai entender e vai dar erro 
+
+# você também pode
+# selecionar várias
+# linhas e apertar no 
+# control + ;
+
+# Para comentar várias linhas após já ter escrito
+
