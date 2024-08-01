3a37aad60f2f60534b4785296e39a03e6e31bfe4
Márcio Luan <marcioluan18@hotmail.com>
2024-08-01 17:12:23 -0300

    Update tratamento_de_erros.py
    adição de mais exemplos

--- tratamento_de_erros.py
+++ tratamento_de_erros.py
@@ -1,29 +1,65 @@
-lista = [1, 2, 3, 4]
-print("\n Impresso antes do erro!\n")
-print(lista[4])
-print("Essa linha é depois do erro")
+# lista = [1, 2, 3, 4]
+# print("\n Impresso antes do erro!\n")
+# print(lista[4])
+# print("Essa linha é depois do erro")
 
+# try:
+#     lista = [1, 2, 3, 4]
+#     print("\n Impresso antes do erro!\n")
+#     print(lista[4])
+#     print("Essa linha é depois do erro")
+# except IndexError as erro:
+#     print("O índice solicitado não existe nessa lista")
 
+# print("oiii")
 
+# v1= [4, 8, 16, 32, 64, 128]
+# v2= [2, 0, 4, 8, 0]
 
+# #vamos efetuar divisão  entre o v1 por v2
 
+# for i in range(len(v1)):
+#     divisao= v1[i]/v2[i]
+#     print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
 
+# v1= [4, 8, 16, 32, 64, 128, 20]
+# v2= [2, 0, 4, 8, 0]
 
+# #Laço for para percorrer a lista a quantidade de elementos que tem na lista v1
+# for i in range(len(v1)):
+#     try:
+#         divisao= v1[i]/v2[i]
+#         print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
+    
+#     #Tratamento do erro da divisão por zero
+#     except (ZeroDivisionError):
+#         print (f"Não é possível dividir um número por zero\n")
+#     #Tratamento do erro de índice da lista
+#     except (IndexError):
+#         print (f"O índice solicitado não existe nessa lista\n")
 
+# v1= [4, 8, 16, 32, 64, 128, 20]
+# v2= [2, 0, 4, 8, 0]
 
+# #Laço for para percorrer a lista a quantidade de elementos que tem na lista v1
+# for i in range(len(v1)):
+#     try:
+#         divisao= v1[i]/v2[i]
+#         print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
+    
+#     #Tratamento do erros erros com a mesma mensagem
+#     except (ZeroDivisionError, IndexError):
+#         print (f"Não é possível dividir um número por zero ou indice não existe na lista\n")
 
+v1= [4, 8, 16, 32, 64, 128, 20]
+v2= [2, 0, 4, 8, 0]
 
-
-
-
-
-
-try:
-    lista = [1, 2, 3, 4]
-    print("\n Impresso antes do erro!\n")
-    print(lista[4])
-    print("Essa linha é depois do erro")
-except IndexError as error:
-    print ("O índice solicitado não existe nessa lista")
-
-print(error)
+#Laço for para percorrer a lista a quantidade de elementos que tem na lista v1
+for i in range(len(v1)):
+    try:
+        divisao= v1[i]/v2[i]
+        print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
+    
+    #Tratamento de erros de maneira genérica
+    except:
+        print (f"Deu erro\n")
