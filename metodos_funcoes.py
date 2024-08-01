0b7b5500d891462a64a83de127bedee0d45c5b4d
Márcio Luan <marcioluan18@hotmail.com>
2024-08-01 20:15:14 -0300

    Create metodos_funcoes.py
    adição de códigos sobre função

---
+++ metodos_funcoes.py
@@ -0,0 +1,28 @@
+# def soma(a , b):
+#     soma = a + b 
+#     return soma
+
+# resultado = soma (10, 8)
+# print (resultado)
+
+# def operacao_x (x, y, z):
+#     resultado = x + y - z
+#     return resultado
+
+# resultado= operacao_x(10, 6, 4)
+# print(resultado)
+
+# def print_nome (nome):
+#     print(f"O nome é: {nome}")
+
+# print_nome("Luan")
+
+# def divisao (x, y):
+#     if y == 0:
+#         return "Não é possível fazer divisão por zero"
+    
+#     else:
+#         return x / y
+    
+# print(divisao(10,5))
+
