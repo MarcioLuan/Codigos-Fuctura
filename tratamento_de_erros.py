68574ef81c02111e7fec841cfc01d6f2d215cc5c
Márcio Luan <marcioluan18@hotmail.com>
2024-08-02 17:40:05 -0300

    adição do bloco finally

--- tratamento_de_erros.py
+++ tratamento_de_erros.py
@@ -47,19 +47,31 @@
 #         divisao= v1[i]/v2[i]
 #         print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
     
-#     #Tratamento do erros erros com a mesma mensagem
-#     except (ZeroDivisionError, IndexError):
-#         print (f"Não é possível dividir um número por zero ou indice não existe na lista\n")
+# #     #Tratamento do erros erros com a mesma mensagem
+# #     except (ZeroDivisionError, IndexError):
+# #         print (f"Não é possível dividir um número por zero ou indice não existe na lista\n")
 
-v1= [4, 8, 16, 32, 64, 128, 20]
-v2= [2, 0, 4, 8, 0]
+# v1= [4, 8, 16, 32, 64, 128, 20]
+# v2= [2, 0, 4, 8, 0]
 
-#Laço for para percorrer a lista a quantidade de elementos que tem na lista v1
-for i in range(len(v1)):
-    try:
-        divisao= v1[i]/v2[i]
-        print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
+# #Laço for para percorrer a lista a quantidade de elementos que tem na lista v1
+# for i in range(len(v1)):
+#     try:
+#         divisao= v1[i]/v2[i]
+#         print (f"O resultado de {v1[i]} dividido por {v2[i]} é {divisao}\n")
     
-    #Tratamento de erros de maneira genérica
-    except:
-        print (f"Deu erro\n")
+#     #Tratamento de erros de maneira genérica
+#     except:
+#         print (f"Deu erro\n")
+
+try:
+    # Código que pode gerar uma exceção
+    resultado = 10 / 0
+
+except ZeroDivisionError:
+    # Código que trata a exceção
+    print("Divisão por zero!")
+    
+finally:
+    # Código que será executado sempre
+    print("Execução do bloco finally.")
